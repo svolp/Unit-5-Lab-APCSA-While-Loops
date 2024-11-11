88cab4d0e05fc62e6dd3886be41cc80425575625
svolp <143809611+svolp@users.noreply.github.com>
2024-11-11 09:20:11 -0500

    Add files via upload

---
+++ MultTable.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+public class MultTable
+{
+	private int number;
+	private int stop;
+
+	public MultTable(int n, int s)
+	{
+	}
+
+	public void printTable( )
+	{
+	}
+}
---
+++ MultTableRunner.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+public class MultTableRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		
+	}
+}
